13863cdf8a62f12c4e3a3e48e0c018de03f97cfe
Nastya Reviako <nastya@reviako.by>
2023-01-30 22:42:58 +0300

    some unit tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,53 @@
 
 namespace Tests\Feature;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Link;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
+    public function test_login()
     {
-        $response = $this->get('/');
+        $response = $this->postJson('/api/login', [
+            'email' => 'niki@ii.com', 
+            'password' => 'niki123123']);
 
-        $response->assertStatus(200);
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => true,
+            ]);
+    }
+
+    public function test_see_all_links()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->getJson('/api/links');    
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => true,
+            ]);
+    }
+
+    public function test_create_a_link()
+    {
+        $user = User::factory()->create();
+
+        $link = Link::factory()->make();
+
+        $response = $this->actingAs($user)->postJson('/api/links', [
+            'originalUrl' => $link['originalUrl'], 
+            'isPublic' => $link['isPublic']
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'status' => true,
+            ]);
     }
 }
